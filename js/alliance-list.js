3092dca0f153b74346eec22ca93be09d47ba19f9
eunicedhivya <divi.seed@gmail.com>
2021-12-01 13:52:18 +0530

    Functionality: add empty selected container and transition

--- js/alliance-list.js
+++ js/alliance-list.js
@@ -1,7 +1,8 @@
 function allianceList(data){
-    console.log(data);
-
+    
     var selectAllListcont = d3.select(".alliance-list")
+
+    selectAllListcont.html("")
 
     var allList = selectAllListcont.selectAll(".alliance-item")
         .data(data).enter().append("li")
@@ -9,4 +10,9 @@ function allianceList(data){
         .html(function(d){
             return d
         })
+        .style("transform", "translateY(-10px)")
+        .style("opacity", 0)
+        .transition().duration(800)
+        .style("transform", "translateY(0)")
+        .style("opacity", 1)
 }
