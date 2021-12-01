712adc6f55bcd5c9cef8ca9e4304c3b963202359
eunicedhivya <divi.seed@gmail.com>
2021-12-01 12:27:05 +0530

    add tranistion and empty data color

--- js/horstackchart.js
+++ js/horstackchart.js
@@ -21,40 +21,44 @@ function drawHorizontalStackChart(selection, stackdata, props) {
         .data(stackdata).enter()
         .append("div").attr("class", "block")
         .style("display",  "inline-block")
+        .style("height", "28px")
+        .transition().duration(800)
         .style("background-color",  function(d,i){
-            console.log(d[props["label"]]);
-            console.log(partycolors["DDS"]);
-            
-            return partycolors[d[props["label"]]];
-
+            if(d[props["label"]] !== "undecided"){
+                return partycolors[d[props["label"]]];
+            }else{
+                return "#e8e8e8"
+            }
         })
         .style("width", function(d,i){
             return d[props["valueper"]]+"%";
         })
-        .style("height", "28px")
-        // .html(function(d,i){
-        //     return '<span class="value">'+d[props["value"]]+'</span> <span class="label">'+d[props["label"]]+'</span>';
-        // })
-        // .attr("title", function(d){
-        //     return d[props["label"]] +": "+ d[props["value"]];
-        // }) //Tooltip
 
     addLegend.selectAll(".legend-block")
         .data(stackdata).enter()
         .append("div").attr("class", "legend-block")
         .style("background-color",  function(d,i){
-            return partycolors[d[props["label"]]];
-
+            if(d[props["label"]] !== "undecided"){
+                return partycolors[d[props["label"]]];
+            }else{
+                return "#e8e8e8"
+            }
         })
         .style("width", "30px")
         .style("height", "8px")
         .style("text-align", "center")
         .style("font-size", "14px")
         .style("display", "inline-block")
-        .style("margin", "0 10px")
         .html(function(d,i){
             return '<span class="partylabel" style="display:block;font-weight:bold;margin: 10px 0 0 0;">'+d[props["label"]]+'</span> <span class="value">'+d[props["value"]]+'</span>';
         })
+        .style("margin", "0 10px 0 10px")
+        .style("transform", "translateY(-20px)")
+        .style("opacity", 0)
+        .transition().duration(800)
+        .style("transform", "translateY(0)")
+        .style("opacity", 1)
+        
         
 
 } // end of horstackchart
